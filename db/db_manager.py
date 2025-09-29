a4e4f294ee87ac8abdd467298b60635842798c69
Natalie D <diatlova.n@ajax.systems>
2025-09-29 15:20:05 +0200

    feat: deleting a linked firewall or rule forbidden

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,11 +1,18 @@
 import logging
 from functools import wraps
 from typing import Union, Optional
+from peewee import DoesNotExist
 
 from .models import db, Firewall, FirewallRule, FilteringPolicy, SqliteDatabase
+from error_handlers import ObjectLinkedError
 
-Models = Union[Firewall, FirewallRule, FilteringPolicy]
+MODELS = Union[Firewall, FirewallRule, FilteringPolicy]
 
+# Columns in FilteringPolicy that reference Firewall and Firewall rule ids
+LINKED_FIELD = {
+    Firewall: FilteringPolicy.firewall_id,
+    FirewallRule: FilteringPolicy.rule_id
+}
 
 class DBManager:
     """
@@ -39,7 +46,7 @@ class DBManager:
         logging.debug('Database init successful')
 
     @db_connection
-    def create_row(self, model: Models, config: dict):
+    def create_row(self, model: MODELS, config: dict):
         try:
             row = model.create(**config)
             row.save()
@@ -50,7 +57,7 @@ class DBManager:
             return row
 
     @db_connection
-    def get_row(self, model: Models, row_id: str, return_field: Optional[str] = None):
+    def get_row(self, model: MODELS, row_id: str, return_field: Optional[str] = None):
         try:
             row = model.select().where(model.id == row_id).get()
         except Exception as error:
@@ -60,7 +67,7 @@ class DBManager:
             return getattr(row, return_field) if return_field else row
 
     @db_connection
-    def get_rows(self, model: Models, row_id: Optional[str] = None):
+    def get_rows(self, model: MODELS, row_id: Optional[str] = None):
         if row_id:
             return [self.get_row(model, row_id)]
 
@@ -73,8 +80,18 @@ class DBManager:
             return rows
 
     @db_connection
-    def delete_row(self, model: Models, row_id: str):
+    def delete_row(self, model: MODELS, row_id: str):
         try:
+            # Forbid user from deleting an object entry that is referenced in some filtering policy
+            # (to avoid database conflicts)
+            if model in {Firewall, FirewallRule}:
+                try:
+                    row = FilteringPolicy.select().where(LINKED_FIELD[model] == row_id).get()
+                    if row:
+                        raise ObjectLinkedError(row_id)
+                except DoesNotExist as error:
+                    logging.warning(f'Object not linked to any policy, error ignored {error}')
+
             query = model.delete().where(model.id == row_id)
             query.execute()
         except Exception as error:
@@ -82,7 +99,7 @@ class DBManager:
             raise
 
     @db_connection
-    def update_row(self, model: Models, row_id: str, config: dict):
+    def update_row(self, model: MODELS, row_id: str, config: dict):
         try:
             query = model.update(**config).where(model.id == row_id)
             query.execute()
--- error_handlers.py
+++ error_handlers.py
@@ -3,17 +3,33 @@ from flask import jsonify, Flask
 from peewee import DoesNotExist
 
 
+class ObjectLinkedError(Exception):
+    def __init__(self, object_id):
+        self.message = f'Object with id {object_id} is linked to an existing Filtering Policy'
+        self.error_code = 409
+
+    def __str__(self):
+        return f'{self.message} (Error Code {self.error_code})'
+
+
 def error_handlers(app: Flask):
     @app.errorhandler(ValueError)
     def handle_value_error(err):
         return jsonify({
-            "error": "value_error",
-            "messages": err.args
+            'error': 'value_error',
+            'messages': err.args
         }), 400
 
     @app.errorhandler(DoesNotExist)
     def handle_value_error(err):
         return jsonify({
-            "error": "not_found",
-            "messages": "Object with given id does not exist"
+            'error': 'not_found',
+            'messages': 'Object with given id does not exist'
         }), 404
+
+    @app.errorhandler(ObjectLinkedError)
+    def handle_object_linked_error(err):
+        return jsonify({
+            'error': 'conflict',
+            'messages': 'Object with given id is linked to a Filtering Policy'
+        }), 409
